e010ee9816406b22a4c8540e8ee501784c3a64c6
btommer2017 <btommer@gmail.com>
2018-03-05 05:48:56 -0600

    added readme

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', " "];
-const songList = ['primary', 'shadowplay', 'lullaby'];
+const songList = ['primary', 'shadowplay', 'lullaby', 'debaser', 'insight', 'caribou',];
 let songPicked = "";
 let lettersInWord = []
 let numBlanks = 0;
